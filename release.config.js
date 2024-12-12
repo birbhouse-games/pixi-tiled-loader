047b2f903a0f7ff0281c4feda2d063826989a5bb
Trezy <tre@trezy.com>
2024-12-12 14:56:24 -0600

    build: use default release plugins

--- release.config.js
+++ release.config.js
@@ -10,15 +10,4 @@ module.exports = {
 			prerelease: true,
 		},
 	],
-	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-		['@semantic-release/npm', {
-			npmPublish: false,
-			tarballDir: 'dist',
-		}],
-		['@semantic-release/github', {
-			assets: 'dist/*.tgz',
-		}],
-	],
-};
+}
